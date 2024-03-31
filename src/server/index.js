f7b017756d059275ca0d3d5e9fe75bf5be53873f
Bobala22 <bogdan.git22@gmail.com>
2024-03-31 12:00:13 +0300

    new_test

--- src/server/index.js
+++ src/server/index.js
@@ -4,7 +4,7 @@ const server = express();
 
 server.post('/api/cegrupasuntem', (req, res) => { // here get would work but in the frontend we do it this way
     console.log("got a new req", req);
-    res.send(JSON.stringify({name: "E1"}));
+    res.send(JSON.stringify({name: "Bogdan"}));
     res.end();
 })
 server.listen(3002, () => console.log('Server started'));
