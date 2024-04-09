b81c8b6a0d372c7c4867ab1092ce641b975e015f
vladsuper5555 <butnaruvlad2003@yahoo.com>
2024-04-09 07:28:27 +0300

    added multiple pages support

--- src/App.jsx
+++ src/App.jsx
@@ -2,29 +2,21 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import Button from '@mui/material/Button';
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'; // Updated import
+import LoginPage from './LoginPage';
+import MainPage from './MainPage';
+import NotFoundPage from './NotFoundPage';
 
 function App() {
-  const [grupa, setGrupa] = useState("");
-
-  async function aflaGrupa () {
-    const res = await new Promise((res) => {
-      fetch('http://localhost:5173/api/cegrupasuntem', {
-        method: "POST",
-        body: ""
-      }).then(async (response) => {
-        res(response.text());
-      })
-    })
-    console.log(res);
-    setGrupa(JSON.parse(res).name);
-  }
   return (
-    <>
-      <div> ce grupa suntem ?</div>
-      <div id = "content"> { grupa } </div>
-      <Button id = 'afla_grupa' variant='contained' onClick = {aflaGrupa}>Afla grupa</Button>
-    </>
+    <Router>
+      <Routes>
+        <Route path="/" element={<MainPage />} /> 
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="*" element={<NotFoundPage />} />
+      </Routes>
+    </Router>
   )
 }
 
