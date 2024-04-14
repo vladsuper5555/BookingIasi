6a60a53a820372e89383663bdc5043f2cfc4b7f4
Alexandru <alex.zosin@yahoo.com>
2024-04-14 14:54:00 +0300

    proposed example for model files

--- src/server/src/models/panoramas.model.js
+++ src/server/src/models/panoramas.model.js
@@ -1,1 +1,20 @@
-// function returning a json object for a given panorama... or something
+/* example
+*a dbconfig.js file might be needed (extra: read about connectionPooling / other efficiency aspects etc.)
+
+import oracledb ...
+function savePanorama(panorama - probably an Object Literal / JSON) {
+    ...
+    'INSERT INTO ...
+}
+
+function getPanorama(id) {
+    ...
+}
+
+export {
+
+}
+
+--- in controller, we import the functions and use them accordingly
+*await/async might be required
+*/
