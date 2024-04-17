75e66d808e9583a4b0322ab596013f336eb08310
Alexandru <alex.zosin@yahoo.com>
2024-04-17 20:59:15 +0300

    further refined the routing with @melintech

--- src/server/src/routes/panoramas/panoramas.controller.js
+++ src/server/src/routes/panoramas/panoramas.controller.js
@@ -1,16 +1,56 @@
-import { getConfig, updateConfig } from '../../models/panoramas.model';
+import {
+    getPanorama,
+    getPanoramaScene,
+    uploadPanorama,
+    updatePanoramaScene,
+    getConfigDEMO // TO DELETE
+} from '../../models/panoramas.model';
 
-function getPanorama(req, res) {
-    res.status(200).json(getConfig());
+function getPanoramaDEMO(req, res) {
+    res.status(200).json(getConfigDEMO());
 }
 
-function updatePanorama(req, res) {
-    const newConfig = req.body;
-    updateConfig(newConfig);
-    res.status(200).json({ message: "Panorama updated!" });
+function httpGetPanorama(req, res) { // panoramas/?hotel=1&room=2
+    const hotel = req.query.hotel;
+    const room = req.query.room;
+
+    try {
+        const panorama = getPanorama(hotel, room);
+        res.status(200).json(panorama);
+    } catch (error) {
+        res.status(404).json({
+            message: error.message
+        });
+    }
+}
+
+function httpGetPanoramaScene(req, res) { // panoramas/(scene)4/?hotel=1&room=2
+    const hotel = req.query.hotel;
+    const room = req.query.room;
+    const scene = req.params.sceneId;
+
+    try {
+        const panoramaScene = getPanoramaScene(hotel, room, scene);
+        res.status(200).json(panoramaScene);
+    } catch (error) {
+        res.status(404).json({
+            message: error.message
+        });
+    }
+}
+
+function httpUploadPanorama(req, res) {
+    uploadPanorama(); // to do
+}
+
+function httpUpdatePanoramaScene(req, res) {
+    updatePanoramaScene(); // to do
 }
 
 export {
-    getPanorama,
-    updatePanorama
+    httpGetPanorama,
+    httpGetPanoramaScene,
+    httpUploadPanorama,
+    httpUpdatePanoramaScene,
+    getPanoramaDEMO // TO DELETE
 };
--- src/server/src/routes/panoramas/panoramas.router.js
+++ src/server/src/routes/panoramas/panoramas.router.js
@@ -1,9 +1,18 @@
 import { Router } from 'express';
-import { getPanorama, updatePanorama } from './panoramas.controller';
+import {
+    httpGetPanorama,
+    httpGetPanoramaScene,
+    httpUploadPanorama,
+    httpUpdatePanoramaScene,
+    getPanoramaDEMO // TO DELETE
+} from './panoramas.controller';
 
 const panoramasRouter = Router();
 
-panoramasRouter.get('/panoramas', getPanorama);
-panoramasRouter.put('/panoramas', updatePanorama);
+panoramasRouter.get('/panoramas', getPanoramaDEMO);
+//panoramasRouter.get('/panoramas', httpGetPanorama);
+panoramasRouter.get('/panoramas/:sceneId', httpGetPanoramaScene);
+panoramasRouter.post('/panoramas', httpUploadPanorama); // middleware authentication/authorize admin
+panoramasRouter.put('/panoramas/:sceneId', httpUpdatePanoramaScene);  // middleware authentication/authorize admin
 
 export default panoramasRouter;
