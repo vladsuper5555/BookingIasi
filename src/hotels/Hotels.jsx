e5774ec8f315342274d503050306c814f52daa15
Bejan Andreea <andreeabejan03@gmail.com>
2024-05-18 15:49:44 +0300

    Added hotels router files and other necesarry folders for hotels page

--- src/hotels/Hotels.jsx
+++ src/hotels/Hotels.jsx
@@ -4,12 +4,16 @@ import { Link, redirect } from 'react-router-dom';
 import { useState } from 'react'
 import Button from '@mui/material/Button';
 import Mesaj from './Mesaj';
+//import mainPhoto from ".images/";//update dupa ce se aleg pozele
+import "./styles/main-page.css";
 
-function Hotels() {
+const Hotels = () => {
   return (
+  <div className='general-stucture'>    
     <div>
       Bine ai venit pe pagina de hoteluri
       <Mesaj/>
+      </div>
     </div>
   );
 }
--- src/server/src/server.js
+++ src/server/src/server.js
@@ -3,8 +3,8 @@ import express from "express"
 import panoramasRouter from './routes/panoramas/panoramas.router.js';
 import userProfileRouter from "./routes/userProfile/userProfile.router.js";
 import attractionsRouter from "./routes/attractionsInfo/attractionsInfo.router.js";
+import hotelsRouter from "./routes/hotels/hotels.router.js";
 import cookieParser from "cookie-parser";
-// import hotelsRouter ... etc.
 import {runQueryOnDatabaseAndFetchEntireResult, runAsyncQueryOnDatabase} from "./models/database.model.js"; 
 
 const server = express();
@@ -18,6 +18,7 @@ server.use(cookieParser());
 server.use('/api', panoramasRouter); // to change to panoramas
 server.use('/api', userProfileRouter);
 server.use('/api', attractionsRouter);
+server.use('/api', hotelsRouter);
 
 
 // TEST PURPOSE ONLY TO BE DELETEDy
