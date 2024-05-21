0649f63e94612b13cbf549e774080e3c37c74c85
Olaru Marius-Alexandru <marius.alexandru.olaru@gmail.com>
2024-05-21 09:11:40 +0300

    update on Login UI testing

--- tests/UI_tests/signupAndLoginTests.ui_test.cjs
+++ tests/UI_tests/signupAndLoginTests.ui_test.cjs
@@ -13,10 +13,14 @@ describe('Signup and Login Test', () => {
     username: faker.internet.displayName(),
     email: faker.internet.email(),
     password: faker.internet.password(),
-  };
+    height: String(faker.datatype.number({ min: 150, max: 200 })), // Height in cm
+    weight: String(faker.datatype.number({ min: 50, max: 120 })), // Weight in kg
+    gender: faker.name.gender(),
+    specialAssistance: faker.datatype.boolean(), // Random boolean for special assistance
+};
 
   beforeAll(async () => {
-    browser = await puppeteer.launch({ headless: true });
+    browser = await puppeteer.launch({ headless: false });
     page = await browser.newPage();
   });
 
@@ -33,23 +37,32 @@ describe('Signup and Login Test', () => {
     await page.type('#email', userData.email);
     await page.type('#password', userData.password);
 
-    await page.click('#submit');
-    await page.waitForNetworkIdle({ idleTime: 300 });
-
-    const responseMessage = await page.$eval('#responseMessage', el => el.textContent);
-    expect(responseMessage).toBeTruthy();
-  });
-
-  it('should log in with the same credentials and display a response message', async () => {
-    await page.goto(`${baseURL}/login`);
-
-    await page.type('#username', userData.username);
-    await page.type('#password', userData.password);
 
     await page.click('#submit');
     await page.waitForNetworkIdle({ idleTime: 300 });
 
-    const responseMessage = await page.$eval('#responseMessage', el => el.textContent);
-    expect(responseMessage).toBeTruthy();
+    // const responseMessage = await page.$eval('#responseMessage', el => el.textContent);
+    // expect(responseMessage).toBeTruthy();
+    const birthDate = faker.date.birthdate({ min: 18, max: 65, mode: 'age' });
+    const formattedBirthDate = `${birthDate.getMonth() + 1}/${birthDate.getDate()}/${birthDate.getFullYear()}`;  
+    await page.type('#Birth_Date', formattedBirthDate);
+    await page.type('#height', userData.height);
+    await page.type('#weight', userData.weight);
+    await page.type('#gender', userData.gender);
+    await page.$eval('#Special_Assistance', (checkbox, specialAssistance) => {
+      if (specialAssistance) {
+        checkbox.checked = true; // If specialAssistance is true, check the checkbox
+      } else {
+        checkbox.checked = false; // If specialAssistance is false, uncheck the checkbox
+      }
+    }, userData.specialAssistance);
+    await page.click('#save-btn');
+    await page.waitForNetworkIdle({ idleTime: 300 });
+
+    const responseMessage = page.url();
+    console.log(responseMessage);
+    expect(responseMessage == 'http://localhost:5173/health-form').toBeTruthy();
   });
+  
+
 });
