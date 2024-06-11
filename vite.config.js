300a7ebd79ec89c59427f265d3e67fe848ebaa49
vladsuper5555 <butnaruvlad2003@yahoo.com>
2024-06-11 16:33:59 +0300

    changed the port

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
     server: {
         host: "0.0.0.0",
-        port: 5173,
+        port: 80,
         proxy: {"/api": "http://51.103.250.250:3002", "/users": "http://20.208.131.253:3002"}
     },
     plugins: [
