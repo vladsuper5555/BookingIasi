702302702578ba79e8520a3f9e2fbb0861e9dcf5
vladsuper5555 <butnaruvlad2003@yahoo.com>
2024-06-11 16:59:24 +0300

    changed the port

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,28 @@
 import {defineConfig} from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
 export default defineConfig({
     server: {
         host: "0.0.0.0",
         port: 80,
-        proxy: {"/api": "http://51.103.250.250:3002", "/users": "http://20.208.131.253:3002"}
+        proxy: {
+            "/api": {
+                target: "http://51.103.250.250:3002",
+                changeOrigin: true,
+                rewrite: (path) => {
+                    console.log('Proxying:', path);
+                    return path;
+                }
+            },
+            "/users": {
+                target: "http://20.208.131.253:3002",
+                changeOrigin: true,
+                rewrite: (path) => {
+                    console.log('Proxying:', path);
+                    return path;
+                }
+            }
+        }
     },
     plugins: [
         react()
