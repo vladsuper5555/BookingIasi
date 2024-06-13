ffadb6183ddf473984eae0b7b7ea85e35fb3c1c1
Rebecabrk <125959502+Rebecabrk@users.noreply.github.com>
2024-06-13 16:40:30 +0300

    userProf responsive

---
+++ src/pointsOfInterest/map.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import MapComponent from './mapComponent';
+
+function App() {
+    return (
+        <div>
+            <h1>Exemplu Google Places API</h1>
+            <MapComponent />
+        </div>
+    );
+}
+
+export default App;
